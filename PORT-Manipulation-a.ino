0344ce0a409d8d9afb85390a54e66f2f076c9c31
wa1tnr <wa1tnr@users.noreply.github.com>
2020-01-31 01:19:36 +0000

    schematic, PA22 for D13, arduino.cc ref

--- PORT-Manipulation-a.ino
+++ PORT-Manipulation-a.ino
@@ -1,1 +1,8 @@
 // Fri Jan 31 00:40:53 UTC 2020
+
+// schematic, Adafruit ItsyBitsyM4 (target):
+// https://learn.adafruit.com/assets/55481
+// PA22 D13
+
+// arduino.cc ref.:
+// https://www.arduino.cc/en/Reference/PortManipulation
