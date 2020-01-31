4502964358c90da9dc2937195129304b651bd594
wa1tnr <wa1tnr@users.noreply.github.com>
2020-01-31 01:20:37 +0000

    comments on arduino.cc stuff

--- PORT-Manipulation-a.ino
+++ PORT-Manipulation-a.ino
@@ -6,3 +6,18 @@
 
 // arduino.cc ref.:
 // https://www.arduino.cc/en/Reference/PortManipulation
+
+/*
+
+DDRB - The Port B Data Direction Register - read/write
+DDRA - The Port A Data Direction Register - read/write (assumed, tnr)
+
+PORTB - The Port B Data Register - read/write
+PORTA - The Port A Data Register - read/write (assumed, tnr)
+
+PINB - The Port B Input Pins Register - read only
+PINA - The Port A Input Pins Register - read only (assumed, tnr)
+
+Note: PINB reminds of Jake Read's stuff.
+
+*/
