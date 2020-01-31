b9a27f8c71b90384e52219f22fa1fa6dca0b98b6
wa1tnr <wa1tnr@users.noreply.github.com>
2020-01-31 01:24:33 +0000

    PA22 - DDRD to DDRA - ag no hits - pins_arduino.h - unsupported method.

--- port_sketch.cpp
+++ port_sketch.cpp
@@ -1,4 +1,21 @@
 #include <Arduino.h>
 
-void setup(void) { }
+void setup(void) {
+    // PA22: D13
+}
+
+/* discussion:
+
+  DDRD would be DDRA.
+  PA22 would be 22 decimal or 21 decimal (zero-ref'd).
+  That's a lot of bits.
+
+  'ag DDRB' in avr shows pins_arduino.h defining.
+
+  No hits in samd.
+
+  Conclusion: not supported.
+
+*/
+
 void  loop(void) { }
