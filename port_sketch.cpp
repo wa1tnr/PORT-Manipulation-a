a368df30698baeac4ac618a2c1d68e2d144c34ae
wa1tnr <wa1tnr@users.noreply.github.com>
2020-01-31 01:42:22 +0000

    REGTYPE uint32_t defined

--- port_sketch.cpp
+++ port_sketch.cpp
@@ -2,6 +2,12 @@
 
 // https://forum.arduino.cc/index.php?topic=334073.0
 
+#ifdef ARDUINO_ARCH_AVR
+  #define REGTYPE uint8_t   // AVR uses 8-bit registers
+#else
+  #define REGTYPE uint32_t
+#endif
+
 void setup(void) {
 
     Serial.begin(9600);
